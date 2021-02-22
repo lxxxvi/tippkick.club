ba65b1b012d5d7d5f1e29e9e90cd9b4b6cab5ead
mario <github@lxxxvi.ch>
2021-02-22 20:06:56 +0100

    create predictions after user create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_save :create_predictions!
+
+  private
+
+  def create_predictions!
+    Game.find_each do |game|
+      Prediction.find_or_create_by(game: game, user: self)
+    end
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test 'valid user' do
     user = User.new(email: 'pele@tippkick.test', password: 'pele')
-    assert user.save!
+
+    assert_difference -> { Prediction.count }, +51 do
+      assert user.save!
+    end
   end
 end
