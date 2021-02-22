5f9ff7bdc0a2489f9a58cda2e20b6aea35c99501
mario <github@lxxxvi.ch>
2021-02-22 19:54:21 +0100

    time validations on predictions

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -7,4 +7,20 @@ class Prediction < ApplicationRecord
             allow_nil: true
 
   validates :game_id, uniqueness: { scope: :user_id }
+
+  validate :scores_cannot_change_after_kickoff
+
+  private
+
+  def scores_cannot_change_after_kickoff
+    return unless scores_changed?
+    return if game.kickoff_at.future?
+
+    errors.add(:home_team_score, :cannot_be_changed_after_kickoff)
+    errors.add(:guest_team_score, :cannot_be_changed_after_kickoff)
+  end
+
+  def scores_changed?
+    home_team_score_changed? || guest_team_score_changed?
+  end
 end
--- test/models/prediction_test.rb
+++ test/models/prediction_test.rb
@@ -9,4 +9,19 @@ class PredictionTest < ActiveSupport::TestCase
 
     assert_includes prediction.errors[:game_id], 'has already been taken'
   end
+
+  test 'validate scores_cannot_change_after_kickoff' do
+    prediction = predictions(:prediction_diego_game_25)
+
+    travel_to prediction.game.kickoff_at - 1.second do
+      assert prediction.update(home_team_score: 9, guest_team_score: 9)
+    end
+
+    travel_to prediction.game.kickoff_at do
+      assert_not prediction.update(home_team_score: 8, guest_team_score: 8)
+
+      assert_includes prediction.errors[:home_team_score], 'cannot be changed after kickoff'
+      assert_includes prediction.errors[:guest_team_score], 'cannot be changed after kickoff'
+    end
+  end
 end
