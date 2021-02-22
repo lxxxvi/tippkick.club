2646de4e6b87026c9405aefe5a450e4496d8efda
mario <github@lxxxvi.ch>
2021-02-22 19:41:45 +0100

    add predictions fixtures and more validations

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -5,4 +5,6 @@ class Prediction < ApplicationRecord
   validates :home_team_score, :guest_team_score, :points,
             numericality: { only_integer: true, greater_than_or_equal_to: 0 },
             allow_nil: true
+
+  validates :game_id, uniqueness: { scope: :user_id }
 end
