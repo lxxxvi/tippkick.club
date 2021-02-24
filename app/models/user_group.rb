5bb7194eb23d9558de1883b96d73fca10c75a774
mario <github@lxxxvi.ch>
2021-02-24 08:02:14 +0100

    add UserGroup.create_with_user
    The class method creates a UserGroup including the initial admin membership of given user.

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -4,4 +4,16 @@ class UserGroup < ApplicationRecord
 
   has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
+
+  def membership_for(user)
+    memberships.find_by(user: user)
+  end
+
+  def self.create_with_user(user, user_group_name)
+    create(name: user_group_name).tap do |user_group|
+      membership = user_group.memberships.find_or_initialize_by(user: user)
+      membership.admin = true
+      user_group.save
+    end
+  end
 end
