d5b606e0ce7c678bd92f62e14fd3e2aee5001d8f
mario <github@lxxxvi.ch>
2021-03-05 16:44:34 +0100

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    "./app/views/**/*"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
