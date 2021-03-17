e36531e8fb8e2fadb5bfc1641005d370ff376010
mario <github@lxxxvi.ch>
2021-03-17 17:52:55 +0100

    extend UserRankingService to calculate the memberships.ranking_positions

--- app/services/user_ranking_service.rb
+++ app/services/user_ranking_service.rb
@@ -1,7 +1,10 @@
 class UserRankingService
   def call!
     User.transaction do
+      execute reset_global_ranking_positions_sql
+      execute reset_memberships_ranking_positions_sql
       execute update_global_ranking_positions_sql
+      execute update_memberships_ranking_positions_sql
     end
   end
 
@@ -9,6 +12,18 @@ class UserRankingService
 
   def execute(statement)
     ApplicationRecord.connection.execute(statement)
+  end
+
+  def reset_global_ranking_positions_sql
+    <<~SQL.squish
+      UPDATE users SET global_ranking_position = NULL
+    SQL
+  end
+
+  def reset_memberships_ranking_positions_sql
+    <<~SQL.squish
+      UPDATE memberships SET ranking_position = NULL
+    SQL
   end
 
   def update_global_ranking_positions_sql
@@ -23,6 +38,25 @@ class UserRankingService
          SET global_ranking_position = ranked_by_total_points.global_ranking_position
         FROM ranked_by_total_points
        WHERE users.id = ranked_by_total_points.id
+    SQL
+  end
+
+  def update_memberships_ranking_positions_sql
+    <<~SQL.squish
+      WITH with_memberships_ranking_position AS (
+        SELECT m.id                                           AS membership_id
+             , u.id                                           AS user_id
+             , u.total_points                                 AS user_total_points
+             , m.user_group_id                                AS user_group_id
+             , RANK() OVER (PARTITION BY m.user_group_id
+                                ORDER BY u.total_points DESC) AS ranking_position
+          FROM memberships m
+          INNER JOIN users u ON u.id = m.user_id
+      )
+      UPDATE memberships
+         SET ranking_position = with_memberships_ranking_position.ranking_position
+        FROM with_memberships_ranking_position
+       WHERE with_memberships_ranking_position.membership_id = memberships.id
     SQL
   end
 end
