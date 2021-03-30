a1be81eeeac2eda992cb38bb4ac54be4029392b0
mario <github@lxxxvi.ch>
2021-03-30 09:10:50 +0200

    add current_user scope to PredictionReflex

--- app/reflexes/application_reflex.rb
+++ app/reflexes/application_reflex.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationReflex < StimulusReflex::Reflex
+  delegate :current_user, to: :connection
   # Put application-wide Reflex behavior and callbacks in this file.
   #
   # Example:
