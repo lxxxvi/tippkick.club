16509155b16f0ccee7343a400ca77cc02b2a6ff3
mario <github@lxxxvi.ch>
2021-04-14 07:32:15 +0200

    add migration to update empty Game.*_team_name to NULL

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_135118) do
+ActiveRecord::Schema.define(version: 2021_04_14_052658) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
