4703279ef9b48f2d2f63adde84a66b26abb47bba
mario <github@lxxxvi.ch>
2021-04-15 08:24:54 +0200

    add translation calls to landing and about page

---
+++ test/components/landing_page_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class LandingPageComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(LandingPageComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
