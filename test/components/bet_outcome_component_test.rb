b4588f6ecc18ed4b4688ff5165cafba06c15039f
mario <github@lxxxvi.ch>
2021-05-01 16:15:47 +0200

    add BetOutcomeComponent

---
+++ test/components/bet_outcome_component_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class BetOutcomeComponentTest < ViewComponent::TestCase
+  test '#render' do
+    game = games(:game_2)
+    component = BetOutcomeComponent.new(game: game)
+    render_inline(component)
+    assert_text '33%'
+    assert_text '67%'
+
+    assert_in_delta component.bets_home_team_wins_percentage, 33.3, 0.1
+    assert_equal 0.0, component.bets_guest_team_wins_percentage
+    assert_in_delta component.bets_draw_percentage, 66.66, 0.1
+  end
+
+  test '#render, not if there are no bets' do
+    game = games(:game_37) # teams or not set for game_37, thus there are not bets
+    component = BetOutcomeComponent.new(game: game)
+    render_inline(component)
+    assert_text ''
+  end
+end
