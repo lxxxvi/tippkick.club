08f041e49a3571d181580e53a1fec471279e3efd
mario <github@lxxxvi.ch>
2021-05-28 14:56:39 +0200

    add clearer information about creating new teams

--- app/components/dashboard_card_component.rb
+++ app/components/dashboard_card_component.rb
@@ -2,4 +2,5 @@ class DashboardCardComponent < ViewComponent::Base
   renders_one :icon
   renders_one :description
   renders_one :link, DashboardCardLinkComponent
+  renders_one :link_addition
 end
