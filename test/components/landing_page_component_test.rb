a8b80db53bcee84c85edd9c50ef40b69672aab0f
mario <github@lxxxvi.ch>
2021-06-12 10:38:30 +0200

    change some texts on the landing page

--- test/components/landing_page_component_test.rb
+++ test/components/landing_page_component_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class LandingPageComponentTest < ViewComponent::TestCase
   test '#render' do
     render_inline LandingPageComponent.new
-    assert_selector 'h1', text: 'We are back!'
+    assert_selector 'h1', text: "Let's go!"
     assert_selector 'h2', text: 'Why join?'
     assert_selector 'h2', text: 'Rules'
   end
