b66cde90d0eb8f14b2612d7f399429deff2f8c2f
mario <github@lxxxvi.ch>
2021-07-11 21:17:30 +0200

    DashboardBetComponent: link to games_path after last game's kickoff

--- test/components/dashboard_bets_component_test.rb
+++ test/components/dashboard_bets_component_test.rb
@@ -6,6 +6,15 @@ class DashboardBetsComponentTest < ViewComponent::TestCase
       component = DashboardBetsComponent.new(users(:diego))
       render_inline(component)
       assert_text 'There are 10 games ready to bet', normalize_ws: true
+      assert_link 'Bets', href: '/tournament/bets'
+    end
+  end
+
+  test '.render, after last games kickoff' do
+    after_kickoff_of_game_51 do
+      component = DashboardBetsComponent.new(users(:diego))
+      render_inline(component)
+      assert_link 'Games', href: '/tournament/games'
     end
   end
 end
