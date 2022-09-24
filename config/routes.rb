07f818461810b2cd581e4261f87a99dda3c6d23a
mario <github@lxxxvi.ch>
2022-09-24 13:17:41 +0200

    wip, small steps

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,25 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope :tournament do
+    resources :bets, only: %i[index]
+    resources :games, only: %i[index]
+  end
 
-  root "landing_page#show"
+  resources :teams do
+    member do
+      get :leave
+      get '/join/:token', to: 'teams#join', as: :join
+    end
+  end
+
+  namespace :admin do
+    resources :games, only: %i[edit update]
+  end
+
+  resource :dashboard, only: %i[show]
+  resource :profile, only: %i[show update destroy]
+
+  get '/about', to: 'about#show'
+  root 'landing_page#show'
 end
